39691f687191cf82af972a5c379b47ed82efce27
markchiu512 <markchiu512@gmail.com>
2023-05-17 00:27:10 +0100

    introduce GreedyAlgorithmV2

--- moo/algorithms/solver.py
+++ moo/algorithms/solver.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING, Dict
+from itertools import permutations
 
 import numpy as np
 
@@ -49,7 +50,7 @@ class GreedyAlgorithmV1:
 
     def solve(self, tsp: 'TravellingSalesmanProblem' ) -> Dict:
         distance_matrix = tsp.distance_matrix
-        # print(distance_matrix)
+         #print(distance_matrix)
 
         # Before salesman travels
         unvisited_cities = list(range(tsp.n_cities))
@@ -80,4 +81,58 @@ class GreedyAlgorithmV1:
         }
         return output_dict
 
+class GreedyAlgorithmV2:
 
+    def solve(self, tsp: 'TravellingSalesmanProblem') -> Dict:
+        distance_matrix = tsp.distance_matrix
+
+        unvisited_cities = np.arange(tsp.n_cities, dtype=np.int16) #[0, 1, 2, 3]
+        unvisited_cities_mask = np.ones(tsp.n_cities, dtype=np.bool_) #[T, T, T, T]
+        travel_order = np.zeros(tsp.n_cities, dtype=np.int16) #(0, 0, 0, 0)
+
+        for i in range(tsp.n_cities-1): 
+            current_city = travel_order[i] #0 #2
+            unvisited_cities_mask[current_city]=False #[F, T, T, T] #[F, T, F, T]
+            current_city_distances_to_all_cities = distance_matrix[current_city, :] #[0, 96, 4.2, 8] #[9, 2, 0, 3]
+            current_city_distances_to_unvisited_cities = current_city_distances_to_all_cities[unvisited_cities_mask] #[96, 4.2, 8] #[2, 3]
+
+            next_city_index = np.argmin(current_city_distances_to_unvisited_cities) #1 #0
+            next_city = unvisited_cities[unvisited_cities_mask][next_city_index] # [1,2,3] [1] = [2] # [1,3] [0] = [1]
+
+            travel_order[i+1] = next_city #[0, 2, 0, 0] #[0, 2, 1, 0]
+
+        objective = tsp.evaluate_travelled_distance(travel_order)
+        output_dict = {
+            'variables': travel_order,
+            'objective': objective
+        }
+        return output_dict
+    
+
+class BruteForceV1:
+
+    def solve(self, tsp: 'TravellingSalesmanProblem'):
+        distance_matrix = tsp.distance_matrix
+        n_cities = tsp.n_cities
+
+        city_permutations = permutations(range(1, n_cities))
+
+        min_distance = float('inf')
+        travel_order = None
+
+        # Calculate the total distance for each permutation
+        for perm in city_permutations:
+            route = [0] + list(perm) + [0]  # Add the starting city to the start and end of the route
+            distance = sum(distance_matrix[route[i-1]][route[i]] for i in range(1, len(route)))
+
+            # Update the minimum distance and best route if this route is better than the current best
+            if distance < min_distance:
+                min_distance = distance
+                travel_order = route
+        
+        output_dict = {
+            'variables': travel_order,
+            'objective': min_distance
+        }
+        return output_dict
+    
